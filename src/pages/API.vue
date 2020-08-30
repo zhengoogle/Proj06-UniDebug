4445769e9834fbd76e9c07d4de0d12f86d9ee34a
刘珍 <llz@163.com>
2020-08-30 20:46:54 +0800

    ci

--- src/pages/API.vue
+++ src/pages/API.vue
@@ -1,11 +1,18 @@
 <template>
-    <div>
-    </div>
+  <div>
+    <!-- ------------------------- divider ------------------------ -->
+    <zz-border-box :height="'30vw'"
+                   :title="'2.'">
+    </zz-border-box>
+    <QRCode></QRCode>
+  </div>
 </template>
 
 <script>
+import QRCode from '../debug/uni/qr-code/QRCode'
 export default {
   name: 'API',
+  components: {QRCode},
   data () {
     return {
       data1: 10
